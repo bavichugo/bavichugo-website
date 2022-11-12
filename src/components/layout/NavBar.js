b0d0e2faabdb9705f785fbaf46443536230f9014
bavichugo <victorhugobatistela@gmail.com>
2022-11-12 11:32:36 -0800

    Add responsive layout for navbar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,27 +1,41 @@
 import { NavLink, Link } from "react-router-dom";
-import { useState } from "react";
 import MainCenteredLayout from "./MainCenteredLayout";
+import { FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons";
 import "./NavBar.scss";
 
 const NavBar = () => {
-  const [highlighted, setHighlighted] = useState();
-
   return (
     <nav className="nav-bar__main-container">
       <MainCenteredLayout className="nav-bar__main-centered-container nav-bar__link-style">
-        <Link
-          to="/"
-          className="nav-bar__my-name"
-        >
+        <Link to="/" className="nav-bar__my-name">
           Victor Hugo Batistela
         </Link>
         <ul className="nav-bar__main-inner-container">
-          <NavLink to="experience">Experience</NavLink>
-          <NavLink to="education">Education</NavLink>
-          <NavLink to="activities">Activities</NavLink>
-          <NavLink to="projects">Projects</NavLink>
-          <NavLink to="contact">Contact</NavLink>
+          <li>
+            <NavLink to="experience">Experience</NavLink>
+          </li>
+          <li>
+            <NavLink to="education">Education</NavLink>
+          </li>
+          <li>
+            <NavLink to="activities">Activities</NavLink>
+          </li>
+          <li>
+            <NavLink to="projects">Projects</NavLink>
+          </li>
+          <li>
+            <NavLink to="contact">Contact</NavLink>
+          </li>
         </ul>
+        <button
+          className="nav-bar__burger-menu"
+          onClick={() => console.log("this was clicked")}
+        >
+          <IconContext.Provider value={{ color: "white" }}>
+            <FaBars />
+          </IconContext.Provider> 
+        </button>
       </MainCenteredLayout>
     </nav>
   );
