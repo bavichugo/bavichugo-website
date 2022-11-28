3d7c483f2e2e4a3d6c3eb1f0b761130b7ff961da
bavichugo <victorhugobatistela@gmail.com>
2022-11-27 18:58:25 -0800

    Fix react particles

--- src/components/layout/CustomParticles.js
+++ src/components/layout/CustomParticles.js
@@ -1,4 +1,6 @@
+import { useCallback } from "react";
 import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 const options = {
   fullScreen: {
@@ -159,9 +161,14 @@ const options = {
 };
 
 const CustomParticles = () => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
   return (
     <Particles
       id="tsparticles"
+      init={particlesInit}
       options={options}
     />
   );
